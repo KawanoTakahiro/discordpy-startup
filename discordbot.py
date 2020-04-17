bbdca31c3de410f66d2c605a2341b448098a5823
KawanoTakahiro <34758826+KawanoTakahiro@users.noreply.github.com>
2020-04-17 11:25:53 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,6 +1,9 @@
 from discord.ext import commands
 import os
 import traceback
+import discord
+from discord.ext import tasks
+from datetime import datetime
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
@@ -16,6 +19,13 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+
+
+async def loop():
+  now = datetime.now().strftime('%H:%M')
+  if now == '13:00':
+    channel = client.get_channel(695294256603988009)
+    await channel_send("筋トレだ、ヤーーーーー！！")
 
 
 bot.run(token)
