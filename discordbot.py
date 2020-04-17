6c95246639e246ab7e674137c5543ab292325c70
KawanoTakahiro <34758826+KawanoTakahiro@users.noreply.github.com>
2020-04-17 14:05:40 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -7,7 +7,7 @@ from discord.ext import tasks
 from datetime import datetime
 
 bot = commands.Bot(command_prefix='/')
-client = discord.Client()
+#client = discord.Client()
 token = os.environ['DISCORD_BOT_TOKEN']
 
 @bot.event
@@ -31,17 +31,5 @@ async def loop():
     if now == '12:00':
         channel = client.get_channel(695294256603988009)
         await channel_send("筋トレだ、ヤーーーーー！！")
-
-@client.event
-async def on_message(message): #メッセージを受け取る関数なので必ず必要
-    if message.content == "きんにくん": #:を忘れずつけよう！Enterを押すと自動で4文字分あけて改行されるよ！
-        await client.send_message(message.channel, "パソコン、Python、パワーーーーーーー！！")
-
-    if message.content == "筋トレ":
-        kaisu = ["10", "15", "20", "25", "30", "35", "40"]
-        syurui = ["腕立て", "腹筋", "背筋", "スクワット", "プランク", "全部"]
-        choice1 = random.choice(kaisu) #randomモジュール使用
-        choice2 = random.choice(syurui) #randomモジュール使用
-        await message.send_message(message.channel, choice2 "を" choice1 "回！ヤーー！！")
 
 bot.run(token)
